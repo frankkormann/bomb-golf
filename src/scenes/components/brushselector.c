14ca46b83a98c4873b016b64cbfc2652b3a598ac
Frank Kormann <frankormann@outlook.com>
2026-04-25 15:53:56 -0400

    Brush selector expand/shrink buttons

--- src/scenes/components/brushselector.c
+++ src/scenes/components/brushselector.c
@@ -6,18 +6,49 @@
 #include "../../rendering/colors.h"
 #include "../../util/dispatcher.h"
 
-#define BUTTON_START_Y 20
+#define BUTTON_START_Y 43
 #define BUTTON_X 1
 #define BUTTON_GAP 13
 
+static bool isOpen;
+static Button expandButton, shrinkButton;
+
 static BrushSelector_Brush brush;
 static Button pencilButton, rectangleButton, ballButton, holeButton;
+
+static void toggleOpen(void *ignored) {
+	isOpen = !isOpen;
+	if (isOpen) {
+		Button_Enable(shrinkButton);
+		Button_Enable(pencilButton);
+		Button_Enable(rectangleButton);
+		Button_Enable(ballButton);
+		Button_Enable(holeButton);
+		Button_Disable(expandButton);
+	} else {
+		Button_Enable(expandButton);
+		Button_Disable(shrinkButton);
+		Button_Disable(pencilButton);
+		Button_Disable(rectangleButton);
+		Button_Disable(ballButton);
+		Button_Disable(holeButton);
+	}
+}
 
 static void setBrush(BrushSelector_Brush newBrush) {
 	brush = newBrush;
 }
 
 bool BrushSelector_Init(BrushSelector_Brush defaultBrush) {
+	expandButton = Button_Create(3, BUTTON_START_Y - 18, SPRITE_BUTTON_RIGHT,
+			NULL, toggleOpen);
+	if (!expandButton) goto f_expandButton;
+	Button_Disable(expandButton);
+
+	shrinkButton = Button_Create(3, BUTTON_START_Y - 18, SPRITE_BUTTON_LEFT,
+			NULL, toggleOpen);
+	if (!shrinkButton) goto f_shrinkButton;
+
 	pencilButton = Button_Create(BUTTON_X, BUTTON_START_Y,
 			SPRITE_PENCIL_BUTTON,
 			(void*)BRUSH_PENCIL, (void(*)(void*))setBrush);
@@ -38,6 +69,8 @@ bool BrushSelector_Init(BrushSelector_Brush defaultBrush) {
 			(void*)BRUSH_HOLE_POS, (void(*)(void*))setBrush);
 	if (!holeButton) goto f_holeButton;
 
+	isOpen = true;
+
 	return true;
 
 f_holeButton:
@@ -47,10 +80,16 @@ f_ballButton:
 f_rectangleButton:
 	Button_Free(pencilButton);
 f_pencilButton:
+	Button_Free(shrinkButton);
+f_shrinkButton:
+	Button_Free(expandButton);
+f_expandButton:
 	return false;
 }
 
 void BrushSelector_Exit() {
+	Button_Free(expandButton);
+	Button_Free(shrinkButton);
 	Button_Free(pencilButton);
 	Button_Free(rectangleButton);
 	Button_Free(ballButton);
@@ -58,6 +97,10 @@ void BrushSelector_Exit() {
 }
 
 bool BrushSelector_RegisterForTouchEvents(Dispatcher touchDispatcher, int priority) {
+	if (!Button_RegisterForTouchEvents(expandButton, touchDispatcher, priority))
+		goto f_expandButton;
+	if (!Button_RegisterForTouchEvents(shrinkButton, touchDispatcher, priority))
+		goto f_shrinkButton;
 	if (!Button_RegisterForTouchEvents(pencilButton, touchDispatcher, priority))
 		goto f_pencilButton;
 	if (!Button_RegisterForTouchEvents(rectangleButton, touchDispatcher,
@@ -77,6 +120,10 @@ f_ballButton:
 f_rectangleButton:
 	Button_RemoveFromTouchDispatcher(pencilButton, touchDispatcher);
 f_pencilButton:
+	Button_RemoveFromTouchDispatcher(shrinkButton, touchDispatcher);
+f_shrinkButton:
+	Button_RemoveFromTouchDispatcher(expandButton, touchDispatcher);
+f_expandButton:
 	return false;
 }
 
@@ -102,6 +149,11 @@ static void drawRectOutline(float x, float y, float depth, float width, float he
 }
 
 void BrushSelector_Draw(float depth) {
+	Button_Draw(expandButton, depth);
+	Button_Draw(shrinkButton, depth);
+
+	if (!isOpen) return;
+
 	Button_Draw(pencilButton, depth);
 	Button_Draw(rectangleButton, depth);
 	Button_Draw(ballButton, depth);
