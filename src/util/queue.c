a40a327b6401407966f29ae60e371b818b75f3fc
Frank Kormann <frankormann@outlook.com>
2026-05-22 16:00:42 -0400

    Switch to linked-list based queue
    Removes the need for rebalancing/resizing the array. A circular
    array implementation was also considered, but it would not be
    faster than the previous array implementation for resizing.
    
    Performance seems about the same between this and the previous
    implementation. This one should be easier to debug.

--- src/util/queue.c
+++ src/util/queue.c
@@ -1,105 +1,70 @@
-//TODO consider changing this implementation to a linked list or circular array
-
 #include <malloc.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include "queue.h"
 
-// If the proportion of unused array space dips below this, a reallocation
-// and rebalance is triggered
-#define REBALANCE_THRESHOLD 0.7
-
-// Proportion of empty space to keep after rebalancing
-#define BUFFER_PROPORTION 0.2
-
-#define MINIMUM_ARR_SIZE 1
+typedef struct node Node;
+struct node {
+	void *val;
+	Node *next;
+};
 
 struct queue {
-	size_t front;
-	size_t back;
-	void **arr;
-	size_t arrSize;
+	Node *head;
+	Node *tail;
 };
 
 Queue Queue_Create() {
 	Queue q = malloc(sizeof(*q));
 	if (!q) return NULL;
 
-	q->arr = malloc(sizeof(void*) * MINIMUM_ARR_SIZE);
-	if (!q->arr) {
-		free(q);
-		return NULL;
-	}
-
-	q->arrSize = MINIMUM_ARR_SIZE;
-	q->front = 0;
-	q->back = 0;
+	q->head = NULL;
+	q->tail = NULL;
 
 	return q;
 }
 
 void Queue_Free(Queue q) {
-	free(q->arr);
+	Node *cur = q->head;
+	while (cur) {
+		Node *next = cur->next;
+		free(cur);
+		cur = next;
+	}
 	free(q);
 }
 
-static void rebalance(Queue q) {
-	size_t capacity = q->front - q->back;
-	if (q->back > 0) {
-		for (size_t i = 0, j = q->back; i < capacity; i++, j++) {
-			q->arr[i] = q->arr[j];
-		}
-	}
-
-	q->back = 0;
-	q->front = capacity;
-
-	size_t newArrSize = capacity * (1 + BUFFER_PROPORTION);
-	if (newArrSize == 0) newArrSize = MINIMUM_ARR_SIZE;
-	if (newArrSize <= q->arrSize) newArrSize = q->arrSize + 1;
-
-	void **newArr = realloc(q->arr, sizeof(void*) * newArrSize);
-	if (newArr) {
-		q->arr = newArr;
-		q->arrSize = newArrSize;
-	}
-}
-
 bool Queue_Push(Queue q, void *elem) {
-	if (q->front >= q->arrSize) {
-		rebalance(q);
-		if (q->front >= q->arrSize) return false;
-	}
+	Node *new = malloc(sizeof(*new));
+	if (!new) return false;
 
-	q->arr[q->front] = elem;
-	q->front++;
+	new->val = elem;
+	new->next = NULL;
+	if (q->tail) q->tail->next = new;
+	q->tail = new;
+	if (!q->head) q->head = new;
+
 	return true;
 }
 
-void* Queue_Peek(Queue q) {
-	return Queue_IsEmpty(q) ? NULL : q->arr[q->back];
-}
-
 void* Queue_Pop(Queue q) {
-	void *elem = Queue_FastPop(q);
-	if (q->front - q->back < REBALANCE_THRESHOLD * q->arrSize) {
-		rebalance(q);
-	}
-	return elem;
+	if (!q->head) return NULL;
+
+	Node *oldHead = q->head;
+	void *rv = oldHead->val;
+	q->head = q->head->next;
+	if (!q->head) q->tail = NULL;
+
+	free(oldHead);
+	return rv;
 }
 
 void* Queue_FastPop(Queue q) {
-	if (Queue_IsEmpty(q)) return NULL;
-
-	void *elem = q->arr[q->back];
-	q->back++;
-	return elem;
+	return Queue_Pop(q);
 }
 
-void Queue_Prune(Queue q) {
-	rebalance(q);
-}
+void Queue_Prune(Queue q) {}
 
 bool Queue_IsEmpty(Queue q) {
-	return q->front == q->back;
+	return !q->head;
 }
