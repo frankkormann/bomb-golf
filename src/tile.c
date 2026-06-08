73b65814fa8352d242abb83e6ca2ed88679797fc
Frank Kormann <frankormann@outlook.com>
2026-06-08 13:48:49 -0400

    New type Tile_WithPos for overlay Tiles

--- src/tile.c
+++ src/tile.c
@@ -1,11 +1,27 @@
+#include <stdbool.h>
 #include <3ds.h>
 #include "tile.h"
 #include "rendering/spritesheet.h"
+
+/*
+ * For Tile:
+ *    Bits 1-3  are orientation flags,
+ *         4-5  are hitbox,
+ *           6  is whether the tile is an overlay or not,
+ *         7-8  are padding/future use,
+ *         9-16 are sprite
+ *
+ * For Tile_WithPos:
+ *    Bits  1-16 are the Tile,
+ *         17-24 are x position (in TILE_SIZE grid coordinates),
+ *         25-32 are y position (in TILE_SIZE grid coordinates)
+ */
 
 Tile_Hitbox spriteToHitbox(SpriteSheet_TileSprite sprite) {
 	switch (sprite) {
 		case SPRITE_TILE_SKY:
 		case SPRITE_TILE_HOUSE_INTERIOR:
+		case SPRITE_TILE_OVERLAY_BOUNCY:
 		case NUM_TILES:
 			return TILE_HITBOX_NONE;
 		case SPRITE_TILE_DIRT:
@@ -35,11 +51,23 @@ Tile_Hitbox spriteToHitbox(SpriteSheet_TileSprite sprite) {
 	return TILE_HITBOX_NONE;
 }
 
-// Bits 1-3 are orientation flags, 4-5 are hitbox, 6-8 are padding/future use,
-// and 8-16 are sprite
+u8 isOverlay(SpriteSheet_TileSprite sprite) {
+	if (sprite == SPRITE_TILE_OVERLAY_BOUNCY) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
 Tile Tile_Make(SpriteSheet_TileSprite sprite, Tile_OrientFlags flags) {
-	return flags | (spriteToHitbox(sprite) << 3) | (sprite << 8);
+	return flags | (spriteToHitbox(sprite) << 3) | (isOverlay(sprite) << 5)
+			| (sprite << 8);
+}
+
+Tile_WithPos Tile_AddPos(Tile tile, int x, int y) {
+	u8 tileX = x / TILE_SIZE;
+	u8 tileY = y / TILE_SIZE;
+	return tile | (tileX << 16) | (tileY << 24);
 }
 
 SpriteSheet_TileSprite Tile_GetSprite(Tile tile) {
@@ -52,4 +80,19 @@ Tile_Hitbox Tile_GetHitbox(Tile tile) {
 
 u8 Tile_GetOrientFlags(Tile tile) {
 	return tile & 0x7;
+}
+
+bool Tile_IsOverlay(Tile tile) {
+	return ((tile >> 5) & 0x1) == 1;
+}
+
+Tile Tile_GetTile(Tile_WithPos tileWithPos) {
+	return tileWithPos & 0xFFFF;
+}
+
+void Tile_GetPos(Tile_WithPos tileWithPos, int *x, int *y) {
+	u8 tileX = (tileWithPos >> 16) & 0xFF;
+	u8 tileY = (tileWithPos >> 24) & 0xFF;
+	*x = tileX * TILE_SIZE;
+	*y = tileY * TILE_SIZE;
 }
--- src/tile.h
+++ src/tile.h
@@ -1,11 +1,16 @@
 /**
  * Make Tiles with a sprite and hitbox. Can be flipped or rotated. Hitbox is
  * inferred from the sprite and orientation.
+ *
+ * The Tile type contains all non-position information for a Tile. The
+ * Tile_WithPos type contains a Tile and position information aligned to a
+ * TILE_SIZE grid.
  */
 
 #ifndef TILE_H
 #define TILE_H
 
+#include <stdbool.h>
 #include <3ds.h>
 #include "rendering/spritesheet.h"
 
@@ -43,11 +48,26 @@ typedef enum {
 } Tile_OrientFlags;
 
 typedef u16 Tile;
+typedef u32 Tile_WithPos;
 
 Tile Tile_Make(SpriteSheet_TileSprite sprite, Tile_OrientFlags flags);
+
+/*
+ * (x, y) must be from (0, 0) to (LEVEL_HEIGHT, LEVEL_MAX_WIDTH). If (x, y) is
+ * not aligned to a TILE_SIZE grid, it will be rounded down.
+ */
+Tile_WithPos Tile_AddPos(Tile tile, int x, int y);
 
 SpriteSheet_TileSprite Tile_GetSprite(Tile tile);
 Tile_Hitbox Tile_GetHitbox(Tile tile);
 u8 Tile_GetOrientFlags(Tile tile);
+
+/*
+ * Returns true if tile should be layered on top of another Tile.
+ */
+bool Tile_IsOverlay(Tile tile);
+
+Tile Tile_GetTile(Tile_WithPos tileWithPos);
+void Tile_GetPos(Tile_WithPos tileWithPos, int *x, int *y);
 
 #endif
