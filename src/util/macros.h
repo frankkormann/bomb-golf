94055c12aead6511378c135c6a3ce23b4859d530
Frank Kormann <frankormann@outlook.com>
2026-09-07 16:50:12 -0400

    Add sign(x)
    For 58fe846f

--- src/util/macros.h
+++ src/util/macros.h
@@ -8,5 +8,6 @@
 #define max(x, y) (((x) > (y)) ? (x) : (y))
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 #define clamp(x, a, b) (min((b), max((x), (a))))
+#define sign(x) ((x) > 0 ? 1 : (x) == 0 ? 0 : -1)
 
 #endif
