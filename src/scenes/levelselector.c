67142c97cba0d22c9aa9d54ea48921ecf5e042be
Frank Kormann <frankormann@outlook.com>
2026-09-12 18:07:56 -0400

    Redesign level selector scene
    Moves level card machinery into the main scene, removes buttons
    on each card, allows for more options for level management

--- src/scenes/levelselector.c
+++ src/scenes/levelselector.c
@@ -1,6 +1,6 @@
-//TODO Redesign this
 #include <stdbool.h>
 #include <malloc.h>
+#include <stdio.h>
 #include <math.h>
 #include <3ds.h>
 #include <citro2d.h>
@@ -10,10 +10,12 @@
 #include "title.h"
 #include "error.h"
 #include "editor.h"
-#include "components/levelcard.h"
+#include "course.h"
 #include "components/background.h"
 #include "components/text.h"
 #include "components/border.h"
+#include "components/popup.h"
+#include "components/button.h"
 #include "../environment/terrain.h"
 #include "../rendering/rendertarget.h"
 #include "../rendering/color.h"
@@ -21,37 +23,243 @@
 #include "../rendering/animation.h"
 #include "../rendering/draw3d.h"
 #include "../util/dispatcher.h"
+#include "../util/tracker.h"
 #include "../levelio.h"
 
 #define LEVEL_NAME_X		10
 #define LEVEL_NAME_Y		15
-
 #define LEVEL_PREVIEW_X		10
 #define LEVEL_PREVIEW_Y		(LEVEL_NAME_Y + 35)
 #define LEVEL_PREVIEW_WIDTH	380
 #define LEVEL_PREVIEW_HEIGHT	90
 
-#define NUM_LEVEL_ROWS		6
-#define NUM_LEVEL_COLUMNS	3
-
-#define CARD_X_START		12
-#define CARD_Y_START		12
-#define CARD_GAP_X		50
-#define CARD_GAP_Y		73
+#define BUTTON_Y		-2
+#define PLAY_BUTTON_X		10
+#define PLAYSEQ_BUTTON_X	60
+#define EDIT_BUTTON_X		162
+#define COPY_BUTTON_X		212
+#define DELETE_BUTTON_X		262
+#define NUM_CARD_ROWS		3
+#define NUM_CARD_COLS		6
+#define CARD_X_START		10
+#define CARD_Y_START		45
+#define CARD_WIDTH		45
+#define CARD_HEIGHT		45
+#define CARD_X_GAP		(CARD_WIDTH + 6)
+#define CARD_Y_GAP		(CARD_HEIGHT + 5)
 
 static Dispatcher touchDispatcher;
-static LevelCard levelCards[NUM_LEVEL_ROWS][NUM_LEVEL_COLUMNS];
+static Button playButton, playSeqButton, editButton, swapButton, deleteButton;
+static Text   playText,   playSeqText,   editText,   swapText,   deleteText;
+
+static Button cards[NUM_CARD_ROWS * NUM_CARD_COLS];
+static Text cardNumbers[NUM_CARD_ROWS * NUM_CARD_COLS];
 
 static Text nameText, parText, infoText;
 static Background levelPreview;
 static LevelIO_Obst *obstacles;
 static size_t numObstacles;
-static bool levelIsSelected;
+static int selectedLevel;
+static bool isLevelLoaded;
 
-static void displayLevel(int levelNum) {
-	if (levelNum < 0) {
+// Declarations needed for buttons
+static void play();
+static void playSequence();
+static void edit();
+static void swap();
+static void delete();
+static void display(int level);
+
+static bool sceneInit(void *sceneParams) {
+	LevelSelector_Params *params = (LevelSelector_Params*)sceneParams;
+
+	touchDispatcher = Dispatcher_Create();
+	if (!touchDispatcher) goto f_touchDispatcher;
+
+	playButton = Button_Create(PLAY_BUTTON_X, BUTTON_Y, SPRITE_SMALL_BUTTON,
+			-1, NULL, play);
+	if (!playButton) goto f_playButton;
+
+	playSeqButton = Button_Create(PLAYSEQ_BUTTON_X, BUTTON_Y,
+			SPRITE_MEDIUM_BUTTON, -1, NULL, playSequence);
+	if (!playSeqButton) goto f_playSeqButton;
+
+	editButton = Button_Create(EDIT_BUTTON_X, BUTTON_Y, SPRITE_SMALL_BUTTON,
+			-1, NULL, edit);
+	if (!editButton) goto f_editButton;
+
+	swapButton = Button_Create(COPY_BUTTON_X, BUTTON_Y, SPRITE_SMALL_BUTTON,
+			-1, NULL, swap);
+	if (!swapButton) goto f_swapButton;
+
+	deleteButton = Button_Create(DELETE_BUTTON_X, BUTTON_Y, SPRITE_SMALL_BUTTON,
+			-1, NULL, delete);
+	if (!deleteButton) goto f_deleteButton;
+
+	Button_RegisterForTouchEvents(playButton, touchDispatcher, 0);
+	Button_RegisterForTouchEvents(playSeqButton, touchDispatcher, 0);
+	Button_RegisterForTouchEvents(editButton, touchDispatcher, 0);
+	Button_RegisterForTouchEvents(swapButton, touchDispatcher, 0);
+	Button_RegisterForTouchEvents(deleteButton, touchDispatcher, 0);
+
+	playText = Text_Create(5);
+	if (!playText) goto f_playText;
+	Text_SetContent(playText, "Play");
+
+	playSeqText = Text_Create(16);
+	if (!playSeqText) goto f_playSeqText;
+	Text_SetContent(playSeqText, "Play In Order");
+
+	editText = Text_Create(5);
+	if (!editText) goto f_editText;
+	Text_SetContent(editText, "Edit");
+
+	swapText = Text_Create(5);
+	if (!swapText) goto f_swapText;
+	Text_SetContent(swapText, "Swap");
+
+	deleteText = Text_Create(7);
+	if (!deleteText) goto f_deleteText;
+	Text_SetContent(deleteText, "Erase");
+	
+	int i = 0;
+	for (int k = 0; k < NUM_CARD_ROWS; k++) {
+		for (int j = 0; j < NUM_CARD_COLS; j++, i++) {
+			float cardX = CARD_X_START + j*CARD_X_GAP;
+			float cardY = CARD_Y_START + k*CARD_Y_GAP;
+			cards[i] = Button_Create(cardX, cardY, SPRITE_LEVEL_CARD,
+					-1, (void*)i, (void(*)(void*))display);
+			if (!cards[i]) goto f_cards;
+			Button_RegisterForTouchEvents(cards[i], touchDispatcher, 0);
+		}
+	}
+
+	int j;
+	for (j = 0; j < NUM_CARD_ROWS * NUM_CARD_COLS; j++) {
+		cardNumbers[j] = Text_Create(3);
+		if (!cardNumbers[j]) goto f_cardNumbers;
+		Text_SetContent(cardNumbers[j], "%i", j + 1);
+	}
+
+	nameText = Text_Create(EDITOR_LEVEL_NAME_MAX + 1);
+	if (!nameText) goto f_nameText;
+
+	parText = Text_Create(9);
+	if (!parText) goto f_parText;
+
+	infoText = Text_Create(32);
+	if (!infoText) goto f_infoText;
+
+	levelPreview = BG_Create(LEVEL_MAX_WIDTH, LEVEL_HEIGHT, COLOR_BLUE);
+	if (!levelPreview) goto f_levelPreview;
+
+	display(params->level);
+
+	return true;
+
+f_levelPreview:
+	Text_Free(infoText);
+f_infoText:
+	Text_Free(parText);
+f_parText:
+	Text_Free(nameText);
+f_nameText:
+f_cardNumbers:
+	for (int k = 0; k < j; k++) Text_Free(cardNumbers[k]);
+f_cards:
+	for (int k = 0; k < i; k++) Button_Free(cards[k]);
+	Text_Free(deleteText);
+f_deleteText:
+	Text_Free(swapText);
+f_swapText:
+	Text_Free(editText);
+f_editText:
+	Text_Free(playSeqText);
+f_playSeqText:
+	Text_Free(playText);
+f_playText:
+	Button_Free(deleteButton);
+f_deleteButton:
+	Button_Free(swapButton);
+f_swapButton:
+	Button_Free(editButton);
+f_editButton:
+	Button_Free(playSeqButton);
+f_playSeqButton:
+	Button_Free(playButton);
+f_playButton:
+	Dispatcher_Free(touchDispatcher);
+f_touchDispatcher:
+	Scene_Switch(sceneError, &(Error_Params) { "Out of memory" });
+	return false;
+}
+
+static void sceneExit() {
+	Text_Free(infoText);
+	Text_Free(parText);
+	Text_Free(nameText);
+	Text_Free(deleteText);
+	Text_Free(swapText);
+	Text_Free(editText);
+	Text_Free(playSeqText);
+	Text_Free(playText);
+	Button_Free(deleteButton);
+	Button_Free(swapButton);
+	Button_Free(editButton);
+	Button_Free(playSeqButton);
+	Button_Free(playButton);
+	Dispatcher_Free(touchDispatcher);
+	BG_Free(levelPreview);
+	if (obstacles) {
+		free(obstacles);
+		obstacles = NULL;
+	}
+	for (int i = 0; i < NUM_CARD_ROWS * NUM_CARD_COLS; i++) {
+		Text_Free(cardNumbers[i]);
+	}
+}
+
+static void play() {
+	if (isLevelLoaded) {
+		Tracker_Clear();
+		Scene_Switch(sceneCourse, &(Course_Params) { selectedLevel, false });
+	}
+}
+
+static void playSequence() {
+	//FIXME
+}
+
+static void edit() {
+	Scene_Switch(sceneEditor, &(Editor_Params) { selectedLevel });
+}
+
+static void swap() {
+	//FIXME
+}
+
+static void doDelete() {
+	char path[LEVEL_PATH_MAX];
+	LevelIO_MakePath(selectedLevel, false, path);
+	remove(path);
+	display(selectedLevel);
+	Popup_Exit();
+}
+
+static void delete() {
+	Popup_Button buttons[] = {
+			{ "Erase", -1, NULL, doDelete },
+			{ "Cancel", KEY_B, NULL, Popup_Exit }
+		};
+	Popup_Init("Really delete? This can't be undone.", POPUP_TWO_BUTTON,
+			buttons);
+}
+
+static void display(int level) {
+	selectedLevel = level;
+	if (level < 0) {
 		Text_SetContent(infoText, "Tap a level number to preview");
-		levelIsSelected = false;
+		isLevelLoaded = false;
 	} else {
 		if (obstacles) {
 			// In case we had it from a previous level selection
@@ -60,7 +268,7 @@ static void displayLevel(int levelNum) {
 		}
 
 		char path[LEVEL_PATH_MAX];
-		LevelIO_MakePath(levelNum, false, path);
+		LevelIO_MakePath(level, false, path);
 		Tile (*tiles)[LEVEL_HEIGHT_TILES];
 		Tile_WithPos *overlayTiles;
 		size_t numOverlayTiles;
@@ -72,7 +280,7 @@ static void displayLevel(int levelNum) {
 				&par, &name, NULL)) {
 			// Spaces to maintain center alignment
 			Text_SetContent(infoText, "Level does not exist");
-			levelIsSelected = false;
+			isLevelLoaded = false;
 			return;
 		}
 
@@ -95,80 +303,7 @@ static void displayLevel(int levelNum) {
 		Text_SetContent(nameText, "%s", name);
 		free(name);
 		Text_SetContent(parText, "Par %i", par);
-		levelIsSelected = true;
-	}
-}
-
-static bool sceneInit(void *sceneParams) {
-	LevelSelector_Params *params = (LevelSelector_Params*)sceneParams;
-	touchDispatcher = Dispatcher_Create();
-	if (!touchDispatcher) goto f_touchDispatcher;
-
-	for (size_t r = 0; r < NUM_LEVEL_ROWS; r++) {
-		for (size_t c = 0; c < NUM_LEVEL_COLUMNS; c++) {
-			levelCards[r][c] = LevelCard_Create(
-					CARD_X_START + (CARD_GAP_X) * r,
-					CARD_Y_START + (CARD_GAP_Y) * c,
-					r + c * NUM_LEVEL_ROWS,
-					displayLevel
-				);
-			if (!levelCards[r][c]) goto f_levelCards;
-			if (!LevelCard_RegisterForTouchEvents(levelCards[r][c],
-					touchDispatcher, 0)) {
-				goto f_levelCards;
-			}
-		}
-	}
-
-	nameText = Text_Create(EDITOR_LEVEL_NAME_MAX + 1);
-	if (!nameText) goto f_nameText;
-
-	parText = Text_Create(9);
-	if (!parText) goto f_parText;
-
-	infoText = Text_Create(32);
-	if (!infoText) goto f_infoText;
-
-	levelPreview = BG_Create(LEVEL_MAX_WIDTH, LEVEL_HEIGHT, COLOR_BLUE);
-	if (!levelPreview) goto f_levelPreview;
-
-	displayLevel(params->level);
-
-	return true;
-
-f_levelPreview:
-	Text_Free(infoText);
-f_infoText:
-	Text_Free(parText);
-f_parText:
-	Text_Free(nameText);
-f_nameText:
-f_levelCards:
-	for (size_t r = 0; r < NUM_LEVEL_ROWS; r++) {
-		for (size_t c = 0; c < NUM_LEVEL_COLUMNS; c++) {
-			if (levelCards[r][c]) LevelCard_Free(levelCards[r][c]);
-		}
-	}
-	Dispatcher_Free(touchDispatcher);
-f_touchDispatcher:
-	Scene_Switch(sceneError, &(Error_Params) { "Out of memory" });
-	return false;
-}
-
-static void sceneExit() {
-	Dispatcher_Free(touchDispatcher);
-	for (size_t r = 0; r < NUM_LEVEL_ROWS; r++) {
-		for (size_t c = 0; c < NUM_LEVEL_COLUMNS; c++) {
-			LevelCard_Free(levelCards[r][c]);
-		}
-	}
-	Text_Free(nameText);
-	Text_Free(parText);
-	Text_Free(infoText);
-	BG_Free(levelPreview);
-	if (obstacles) {
-		free(obstacles);
-		obstacles = NULL;
+		isLevelLoaded = true;
 	}
 }
 
@@ -207,6 +342,16 @@ static void drawObstacle(LevelIO_Obst obst, float previewX, float previewY,
 	C2D_ViewRestore(&prevMtx);
 }
 
+static void drawOutline(int x, int y, float depth, int width, int height,
+		u32 color, int outlineWidth) {
+	C2D_DrawRectSolid(x+1, y, depth, width-2, outlineWidth, color);
+	C2D_DrawRectSolid(x, y+1, depth, outlineWidth, height-2, color);
+	C2D_DrawRectSolid(x+1, y + height - outlineWidth, 0, width-2, outlineWidth,
+			color);
+	C2D_DrawRectSolid(x + width - outlineWidth, y+1, 0, outlineWidth, height-2,
+			color);
+}
+
 static void sceneDraw() {
 	BG_UpdateGraphics(levelPreview);
 
@@ -224,7 +369,7 @@ static void sceneDraw() {
 	C2D_TargetClear(D3D_TARGET, COLOR_LGRAY); \
 	C2D_SceneBegin(D3D_TARGET); \
 	\
-	if (levelIsSelected) { \
+	if (isLevelLoaded) { \
 		Text_Draw(nameText, D3D_Xi(0), LEVEL_NAME_Y, D3D_D(0), \
 				COLOR_DGREEN, 1, TEXT_LEFT); \
 		Text_Draw(parText, D3D_Xi(1), LEVEL_NAME_Y, D3D_D(1), COLOR_DGREEN, \
@@ -254,11 +399,38 @@ static void sceneDraw() {
 	C2D_TargetClear(bottom, COLOR_LGRAY);
 	C2D_SceneBegin(bottom);
 
-	for (size_t r = 0; r < NUM_LEVEL_ROWS; r++) {
-		for (size_t c = 0; c < NUM_LEVEL_COLUMNS; c++) {
-			LevelCard_Draw(levelCards[r][c], 0);
+	Button_Draw(playButton, 0);
+	Button_Draw(playSeqButton, 0);
+	Button_Draw(editButton, 0);
+	Button_Draw(swapButton, 0);
+	Button_Draw(deleteButton, 0);
+	Text_Draw(playText, PLAY_BUTTON_X + 24, BUTTON_Y + 5, 0.5, COLOR_LGRAY,
+			1, TEXT_CENTER);
+	Text_Draw(playSeqText, PLAYSEQ_BUTTON_X + 50, BUTTON_Y + 5, 0.5, COLOR_LGRAY,
+			1, TEXT_CENTER);
+	Text_Draw(editText, EDIT_BUTTON_X + 24, BUTTON_Y + 5, 0.5, COLOR_LGRAY,
+			1, TEXT_CENTER);
+	Text_Draw(swapText, COPY_BUTTON_X + 24, BUTTON_Y + 5, 0.5, COLOR_LGRAY,
+			1, TEXT_CENTER);
+	Text_Draw(deleteText, DELETE_BUTTON_X + 24, BUTTON_Y + 5, 0.5, COLOR_LGRAY,
+			1, TEXT_CENTER);
+
+	for (int i = 0; i < NUM_CARD_COLS; i++) {
+		for (int j = 0; j < NUM_CARD_ROWS; j++) {
+			float cardX = CARD_X_START + i*CARD_X_GAP;
+			float cardY = CARD_Y_START + j*CARD_Y_GAP;
+			Button_Draw(cards[i + j*NUM_CARD_COLS], 0);
+			Text_Draw(cardNumbers[i + j*NUM_CARD_COLS], cardX + 10,
+					cardY + 5, 0.5, COLOR_LGRAY, 1, TEXT_LEFT);
 		}
 	}
+
+	if (selectedLevel >= 0) {
+		int x = CARD_X_START + (selectedLevel % NUM_CARD_COLS) * CARD_X_GAP;
+		int y = CARD_Y_START + (selectedLevel / NUM_CARD_COLS) * CARD_Y_GAP;
+		drawOutline(x, y, 0.5, CARD_WIDTH, CARD_HEIGHT, COLOR_DRED, 2);
+	}
+
 	Animation_Draw(0.5);
 }
 
