1fd08a33014cc4578ef601228df4541694b1c0cb
Frank Kormann <frankormann@outlook.com>
2026-09-14 17:57:50 -0400

    Improve layout if there are fewer than 18 levels

--- src/scenes/summary.c
+++ src/scenes/summary.c
@@ -1,4 +1,4 @@
-#include <malloc.h>
+#include <math.h>
 #include <stdbool.h>
 #include <3ds.h>
 #include <citro2d.h>
@@ -28,7 +28,7 @@
 #define SCORES_NAME_WIDTH	200
 
 #define SCORES_TOP_X		85
-#define SCORES_TOP_Y_START	30
+#define SCORES_TOP_Y_START	25
 
 #define SCORES_BOT_X		45
 #define SCORES_BOT_Y_START	5
@@ -37,9 +37,9 @@
 	(SCORES_BOT_Y_START + TEXT_LINE_HEIGHT*8 + BORDER_MARGIN_VERT + 10)
 
 #define SCORE_REVEAL_TIME	15
-#define TIMER_MAX		(18*SCORE_REVEAL_TIME + 1)
 
 static Text nameText[18], scoreText[18], bottomText, totalScoreText, killCountText;
+static int numScores;
 
 static u32 bgColor, fgColor;
 static int timer;
@@ -49,25 +49,24 @@ static bool sceneInit(void *sceneParams) {
 	Summary_Params *params = (Summary_Params*)sceneParams;
 
 	int i;
+	numScores = 0;
 	for (i = 0; i < 18; i++) {
-		nameText[i] = Text_Create(EDITOR_LEVEL_NAME_MAX + 1);
-		if (!nameText[i]) goto f_nameText;
-
 		char *s, path[LEVEL_PATH_MAX];
 		LevelIO_MakePath(i, params->inRomfs, path);
 		if (LevelIO_ReadName(path, &s)) {
-			Text_SetContent(nameText[i], s);
-			free(s);
-		} else {
-			Text_SetContent(nameText[i], "");
-		}
-	}
+			nameText[i] = Text_Create(EDITOR_LEVEL_NAME_MAX + 1);
+			if (!nameText[i]) goto f_nameScoreText;
+			scoreText[i] = Text_Create(4);
+			if (!scoreText[i]) goto f_nameScoreText;
 
-	int j;
-	for (j = 0; j < 18; j++) {
-		scoreText[j] = Text_Create(4);
-		if (!scoreText[j]) goto f_scoreText;
-		Text_SetContent(scoreText[j], "%+i", Tracker_Get(TRACKER_LVL1 + j));
+			Text_SetContent(nameText[i], s);
+			Text_SetContent(scoreText[i], "%+i",
+					Tracker_Get(TRACKER_LVL1 + i));
+			numScores++;
+		} else {
+			nameText[i] = NULL;
+			scoreText[i] = NULL;
+		}
 	}
 
 	bottomText = Text_Create(32);
@@ -109,18 +108,19 @@ f_killCountText:
 f_totalScoreText:
 	Text_Free(bottomText);
 f_bottomText:
-f_scoreText:
-	for (int k = 0; k < j; k++) Text_Free(scoreText[k]);
-f_nameText:
-	for (int k = 0; k < i; k++) Text_Free(nameText[k]);
+f_nameScoreText:
+	for (int j = 0; j < i; j++) {
+		if (nameText[j]) Text_Free(nameText[j]);
+		if (scoreText[j]) Text_Free(scoreText[j]);
+	}
 	Scene_Switch(sceneError, &(Error_Params) { "Out of memory" });
 	return false;
 }
 
 static void sceneExit() {
 	for (int i = 0; i < 18; i++) {
-		Text_Free(nameText[i]);
-		Text_Free(scoreText[i]);
+		if (nameText[i]) Text_Free(nameText[i]);
+		if (scoreText[i]) Text_Free(scoreText[i]);
 	}
 	Text_Free(bottomText);
 	Text_Free(totalScoreText);
@@ -131,15 +131,16 @@ static void sceneExit() {
 static void sceneUpdate(float _) {
 	u32 kDown = hidKeysDown();
 
-	timer = clamp(timer + 1, 0, TIMER_MAX);
-	if (timer % SCORE_REVEAL_TIME == 0 && timer / SCORE_REVEAL_TIME < 18) {
+	timer = clamp(timer + 1, 0, (numScores + 1) * SCORE_REVEAL_TIME);
+	if (timer % SCORE_REVEAL_TIME == 0
+			&& timer / SCORE_REVEAL_TIME < numScores) {
 		SoundEffect_Play(SFX_BOUNCE, true);
 	}
 
 	if (kDown & (KEY_A | KEY_B | KEY_X | KEY_Y)
 			|| TouchInput_JustFinished()) {
-		if (timer < TIMER_MAX) {
-			timer = TIMER_MAX;
+		if (timer < (numScores + 1) * SCORE_REVEAL_TIME) {
+			timer = (numScores + 1) * SCORE_REVEAL_TIME;
 		} else if (inRomfs) {
 			Scene_Switch(sceneCredits,
 					&(Credits_Params) SCENE_PARAMS_EMPTY);
@@ -160,6 +161,8 @@ static u32 getColorForScore(int score) {
 }
 
 static void sceneDraw() {
+	int i, numDrawn;
+
 	#define D3D_VALS { \
 			{ SCORES_TOP_X, 0.8 }, \
 			{ SCORES_TOP_X + SCORES_WIDTH, 0.8 }, \
@@ -169,20 +172,32 @@ static void sceneDraw() {
 	C2D_TargetClear(D3D_TARGET, bgColor); \
 	C2D_SceneBegin(D3D_TARGET); \
 	\
-	C2D_DrawRectSolid(D3D_Xi(2), SCORES_TOP_Y_START - BORDER_MARGIN_VERT, -1, \
-			SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ, 240, COLOR_LGRAY); \
-	for (int i = 0; i < 10; i++) { \
-		if (timer < SCORE_REVEAL_TIME*i) break; \
+	i = 0; \
+	for (numDrawn = 0; numDrawn < 10 && i < 18; i++) { \
+		if (!nameText[i] || !scoreText[i]) continue; \
 		u32 scoreColor = getColorForScore(Tracker_Get(TRACKER_LVL1 + i)); \
-		Text_DrawBounded(nameText[i], D3D_Xi(0), \
-				SCORES_TOP_Y_START + i*TEXT_LINE_HEIGHT, D3D_D(0), \
-				SCORES_NAME_WIDTH, COLOR_DGREEN, 1, TEXT_LEFT); \
-		Text_Draw(scoreText[i], D3D_Xi(1), \
-				SCORES_TOP_Y_START + i*TEXT_LINE_HEIGHT, D3D_D(1), \
-				scoreColor, 1, TEXT_RIGHT); \
+		if (timer >= numDrawn * SCORE_REVEAL_TIME) { \
+			int y = SCORES_TOP_Y_START + numDrawn*TEXT_LINE_HEIGHT; \
+			/* Make sure the background looks good underneath the \
+			   text, sometimes there are issues due to transparency */ \
+			C2D_DrawRectSolid(D3D_Xi(2), y, D3D_D(2), \
+					SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ, \
+					TEXT_LINE_HEIGHT, COLOR_LGRAY); \
+			Text_DrawBounded(nameText[i], D3D_Xi(0), y, D3D_D(0), \
+					SCORES_NAME_WIDTH, COLOR_DGREEN, 1, \
+					TEXT_LEFT); \
+			Text_Draw(scoreText[i], D3D_Xi(1), y, D3D_D(1), scoreColor, \
+					1, TEXT_RIGHT); \
+		} \
+		numDrawn++; \
 	} \
+	int boxHeight = (numDrawn * TEXT_LINE_HEIGHT) + 2*BORDER_MARGIN_VERT; \
+	if (numDrawn < numScores) boxHeight += 20; \
+	C2D_DrawRectSolid(D3D_Xi(2), SCORES_TOP_Y_START - BORDER_MARGIN_VERT, \
+			D3D_D(2), SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ, \
+			boxHeight, COLOR_LGRAY); \
 	Border_DrawLight(D3D_Xi(2), SCORES_TOP_Y_START - BORDER_MARGIN_VERT, \
-			D3D_D(2), SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ, 240);
+			D3D_D(2), SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ, boxHeight);
 	#include "../rendering/draw3d_gen.h"
 	/* Everything gets #undef'd by draw3d */
 
@@ -191,26 +206,37 @@ static void sceneDraw() {
 	C2D_TargetClear(bottom, bgColor);
 	C2D_SceneBegin(bottom);
 
-	C2D_DrawRectSolid(SCORES_BOT_X - BORDER_MARGIN_HORIZ, 0, 0,
-			SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ,
-			SCORES_BOT_Y_START + 8*TEXT_LINE_HEIGHT
-				+ BORDER_MARGIN_VERT, COLOR_LGRAY);
-	for (int i = 0; i < 8; i++) {
-		if (timer < SCORE_REVEAL_TIME*(i + 10)) break;
-		u32 scoreColor = getColorForScore(Tracker_Get(TRACKER_LVL1 + i+10));
-		Text_DrawBounded(nameText[i+10], SCORES_BOT_X,
-				SCORES_BOT_Y_START + i*TEXT_LINE_HEIGHT, 0,
-				SCORES_NAME_WIDTH, COLOR_DGREEN, 1, TEXT_LEFT);
-		Text_Draw(scoreText[i+10], SCORES_BOT_X + SCORES_WIDTH,
-				SCORES_BOT_Y_START + i*TEXT_LINE_HEIGHT, 0,
-				scoreColor, 1, TEXT_RIGHT);
+	int topTime = numDrawn * SCORE_REVEAL_TIME;
+	for (numDrawn = 0; numDrawn < 8 && i < 18; i++) {
+		if (!nameText[i] || !scoreText[i]) continue;
+		u32 scoreColor = getColorForScore(Tracker_Get(TRACKER_LVL1 + i));
+		if (timer >= numDrawn * SCORE_REVEAL_TIME + topTime) {
+			int y = SCORES_BOT_Y_START + numDrawn*TEXT_LINE_HEIGHT;
+			// Make sure the background looks good underneath the
+			// text, sometimes there are issues due to transparency
+			C2D_DrawRectSolid(SCORES_BOT_X - BORDER_MARGIN_HORIZ, y, 0,
+					SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ,
+					TEXT_LINE_HEIGHT, COLOR_LGRAY);
+			Text_DrawBounded(nameText[i], SCORES_BOT_X, y, 0.5,
+					SCORES_NAME_WIDTH, COLOR_DGREEN, 1,
+					TEXT_LEFT);
+			Text_Draw(scoreText[i], SCORES_BOT_X + SCORES_WIDTH, y,
+					0.5, scoreColor, 1, TEXT_RIGHT);
+		}
+		numDrawn++;
 	}
-	Border_DrawLight(SCORES_BOT_X - BORDER_MARGIN_HORIZ, 0, 0,
-			SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ,
-			SCORES_BOT_Y_START + 8*TEXT_LINE_HEIGHT
-				+ BORDER_MARGIN_VERT);
+	if (numDrawn > 0) {
+	int boxHeight = (numDrawn * TEXT_LINE_HEIGHT) + 2*BORDER_MARGIN_VERT;
+	C2D_DrawRectSolid(SCORES_BOT_X - BORDER_MARGIN_HORIZ,
+			SCORES_BOT_Y_START - BORDER_MARGIN_VERT, 0,
+			SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ, boxHeight,
+			COLOR_LGRAY);
+	Border_DrawLight(SCORES_BOT_X - BORDER_MARGIN_HORIZ,
+			SCORES_BOT_Y_START - BORDER_MARGIN_VERT, 0,
+			SCORES_WIDTH + 2*BORDER_MARGIN_HORIZ, boxHeight);
+	}
 
-	if (timer >= SCORE_REVEAL_TIME*18) {
+	if (timer >= SCORE_REVEAL_TIME * (numScores + 1)) {
 		Text_Draw(bottomText, OVERALL_X, OVERALL_Y, 0, fgColor, 1,
 				TEXT_LEFT);
 		Text_Draw(totalScoreText, OVERALL_X + SCORES_WIDTH, OVERALL_Y, 0,
